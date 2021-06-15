e9786263b09bfe9b3b75fb9a9a414d37b0555898
Marcio Gadelha <zagadelha@gmail.com>
2021-06-15 12:27:27 -0300

    Clean code

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,17 +12,4 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-//	@Bean
-//	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-//		System.out.println("Entrei no GATEWAY!!");
-//		return builder.routes().route(p -> p.path("/").uri("http://localhost:8765/cb-core/*")).build();
-//	}
-
-//	zull.routes.auth.service-id=cb-auth
-//	zull.routes.auth.path=/cb-auth
-//	zull.routes.config.service-id=cb-config
-//	zull.routes.config.path=/cb-config/**
-//	zull.routes.core.service-id=cb-core
-//	zull.routes.core.path=/cb-core/**
-
 }
